61fd04cdc35097c8ede1f16dbf3e87074868421c
unknown <vinaykushwah890@gmail.com>
2025-01-28 11:59:44 +0530

    learn Pattern

---
+++ Patterns/index.js
@@ -0,0 +1,20 @@
+// Square Pattern
+
+// function printSquarePattern(size) {
+//   for (let i = 0; i < size; i++) {
+//     let row = "";
+//     for (let j = 0; j < size; j++) {
+//       //   row += "* ";
+//       //   row += i + " ";
+//     }
+//     console.log(row);
+//   }
+// }
+
+// printSquarePattern(5); // Example usage
+
+// Pramid Pattern
+
+function printPramidPattern(n) {
+  for (let i = 0; i < n; i++) {}
+}
